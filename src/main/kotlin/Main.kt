e9d291b156ee75639c9fba5198252fbcaa209a25
Anton Popovich <a@redmercury.dev>
2022-12-18 15:25:25 +0100

    Чтение и запись в файл.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+import java.io.File
+import java.io.PrintWriter
+import java.util.*
 import kotlin.math.abs
 
 // TODO переделать на формат, когда читается входной файл с парами матчей и результатами (если есть).
@@ -10,9 +13,9 @@ import kotlin.math.abs
 //  сделать простой формат указания результата матча (с любым возможным разделителем, не только двоеточие, а по дефолту — минус),
 //  чтобы игроки сами могли легко это заполнять, и объяснить перед турниром, как этим пользоваться
 
-// TODO НОВЫЕ матчи (назначенные только что) в файле помечаются "--->" в начале строки
-
 // TODO файл должен быть полностью на русском (и имена, и конфигурация, и комментарии)
+
+// TODO НОВЫЕ матчи (назначенные только что) в файле помечаются "--->" в начале строки
 
 // TODO добавить при запуске вывод текущей таблицы (со всеми очками, сетами, местами, средними очками, средними сетами
 //  и матрицей сыгранных игр), а не только предложение новых пар
@@ -60,48 +63,90 @@ fun generateNextMatch(allPlayers: List<Player>, tournamentMatchesPerPlayerCnt: I
     return null
 }
 
-fun main() {
-
-    val players = listOf(
-        Player("A"),
-        Player("B"),
-        Player("C"),
-        Player("D"),
-        Player("E"),
-        Player("F"),
-        Player("G"),
-        Player("H"),
-        Player("I"),
-        Player("J"),
-        Player("K"),
-        Player("L"),
-    )
-
-    val match1 = generateAndStartMatch(players)
-    val match2 = generateAndStartMatch(players)
-    generateAndStartMatch(players)
-    generateAndStartMatch(players)
-    endMatch(match1!!, 3, 0)
-    generateAndStartMatch(players)
-    endMatch(match2!!, 3, 1)
-    generateAndStartMatch(players)
-    generateAndStartMatch(players)
-}
-
-private fun generateAndStartMatch(players: List<Player>): Pair<Player, Player>? {
-    val match = generateNextMatch(players, 8)
-    match?.let { startMatch(it) } ?: println("Не удалось сгенерировать матч")
-    return match
-}
-
 fun startMatch(match: Pair<Player, Player>) {
-    println("Начинаем матч: ${match.first} — ${match.second}")
     match.first.startMatchWith(match.second.name)
     match.second.startMatchWith(match.first.name)
 }
 
-fun endMatch(match: Pair<Player, Player>, sets1: Int, sets2: Int) {
-    println("Заканчиваем матч: ${match.first} — ${match.second} ($sets1:$sets2)")
-    match.first.endMatch(sets1, sets2)
-    match.second.endMatch(sets2, sets1)
+fun endMatch(match: Pair<Player, Player>, sets: Pair<Int, Int>) {
+    match.first.endMatch(sets.first, sets.second)
+    match.second.endMatch(sets.second, sets.first)
+}
+
+private fun generateAndStartMatch(players: List<Player>, tournamentMatchesPerPlayerCnt: Int): Pair<Player, Player>? {
+    return generateNextMatch(players, tournamentMatchesPerPlayerCnt)?.also { startMatch(it) }
+}
+
+/**
+ * Возвращает, был ли начат новый незавершённый матч.
+ */
+private fun parseMatchLine(allPlayers: ArrayList<Player>, line: String): Boolean {
+    val tok = line.split(" ")
+
+    val player1 = allPlayers.first { it.name == tok[0] }
+    val player2 = allPlayers.first { it.name == tok[1] }
+
+    // начинаем матч
+    startMatch(player1 to player2)
+
+    if (tok.size == 2) { // незавершённый матч
+        return true
+    } else if (tok.size == 4) { // результаты матча
+        val sets1 = line.split(" ")[2].toInt()
+        val sets2 = line.split(" ")[3].toInt()
+        endMatch(player1 to player2, sets1 to sets2)
+        return false
+    } else {
+        throw Exception("Неверный формат строки: '$line'")
+    }
+}
+
+private const val GO_TO_TABLE_PREFIX = "К СТОЛУ --> "
+
+fun main() {
+    var tablesCnt = 1
+    var tablesOccupied = 0
+    var tournamentMatchesPerPlayerCnt = 1
+    val allPlayers = ArrayList<Player>()
+
+    val inputFile = File("tournament.txt")
+    val sc = Scanner(inputFile)
+
+    val outputFile = File("tournament_out.txt")
+    val pw = PrintWriter(outputFile)
+
+    while (sc.hasNextLine()) {
+        val lineRaw = sc.nextLine().removePrefix(GO_TO_TABLE_PREFIX)
+        pw.println(lineRaw)
+
+        val line = lineRaw.trim()
+        if (line.isBlank()) { // пропускаем пустые строки
+            continue
+        } else if (line.startsWith("#")) { // пропускаем комменты
+            continue
+        } else if (line.split(" ").first().let { name -> allPlayers.count { it.name == name } > 0 }) {
+            // если игрок уже есть в списке, то просто добавляем результаты
+            if (parseMatchLine(allPlayers, line)) tablesOccupied++
+        } else if (line.lowercase().startsWith("Стол".lowercase())) {
+            tablesCnt = line.split(" ").last().toInt()
+        } else if (line.lowercase().startsWith("Матч".lowercase())) {
+            tournamentMatchesPerPlayerCnt = line.split(" ").last().toInt()
+        } else if (line.lowercase().startsWith("Игрок".lowercase())) {
+            allPlayers += Player(line.split(" ").last())
+        } else {
+            throw IllegalArgumentException("Не могу разобрать строку: '$line'")
+        }
+    }
+    sc.close()
+
+    for (i in tablesOccupied until tablesCnt) {
+        generateAndStartMatch(allPlayers, tournamentMatchesPerPlayerCnt)?.let { match ->
+            pw.println("$GO_TO_TABLE_PREFIX${match.first.name} ${match.second.name}")
+        } ?: break
+    }
+    pw.close()
+
+    inputFile.delete()
+    outputFile.renameTo(inputFile)
+
 }
