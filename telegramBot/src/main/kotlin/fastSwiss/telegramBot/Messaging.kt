e2be392b7ba51418f85e89251f3a0bf3a03691d0
Anton Popovich <a@redmercury.dev>
2023-02-02 21:12:12 +0000

    Вывожу настройки турнира, либо сетку, в зависимости от того, стартовал турнир или нет.

--- telegramBot/src/main/kotlin/fastSwiss/telegramBot/Messaging.kt
+++ telegramBot/src/main/kotlin/fastSwiss/telegramBot/Messaging.kt
@@ -11,20 +11,21 @@ import dev.inmo.tgbotapi.types.message.content.MessageContent
 import dev.inmo.tgbotapi.types.message.content.TextContent
 import dev.inmo.tgbotapi.utils.EntitiesBuilderBody
 import dev.inmo.tgbotapi.utils.buildEntities
+import dev.inmo.tgbotapi.utils.code
 import fastSwiss.api.IncorrectChangeException
 import fastSwiss.api.MutableTournament
 import kotlinx.coroutines.flow.firstOrNull
 
 
 suspend fun <T> BehaviourContext.processDialog(
-    message: ContentMessage<TextContent>,
+    message: ContentMessage<MessageContent>,
     botText: String,
     replyMarkup: KeyboardMarkup? = null,
     answerConverter: (CommonMessage<MessageContent>) -> T?,
     logic: (T) -> Unit,
     formatAnswer: (T) -> EntitiesBuilderBody,
     tournamentInfoToOutput: MutableTournament<*>?,
-): ContentMessage<TextContent>? {
+): Pair<CommonMessage<MessageContent>, ContentMessage<TextContent>>? {
     reply(
         to = message,
         text = botText,
@@ -40,10 +41,10 @@ suspend fun <T> BehaviourContext.processReply(
     logic: (T) -> Unit,
     formatAnswer: (T) -> EntitiesBuilderBody,
     tournamentInfoToOutput: MutableTournament<*>?,
-): ContentMessage<TextContent>? {
+): Pair<CommonMessage<MessageContent>, ContentMessage<TextContent>>? {
     val message = waitContentMessage().firstOrNull()
     val answer = (message?.let { answerConverter(it) } ?: return null) as T
-    return try {
+    return message to try {
         logic(answer)
         reply(message, buildEntities("", formatAnswer(answer))).also {
             tournamentInfoToOutput?.let { tournament -> outputTournamentInfoMessage(it, tournament) }
@@ -54,24 +55,27 @@ suspend fun <T> BehaviourContext.processReply(
 }
 
 
-// TODO вывод текущей инфы о турнире
-//      если он ещё не начат — то настройки и список игроков, а также дока по запуску турнира
-//      если он уже начат — то таблица и матчи, которые сейчас играются, а также настройки и дока по продолжению турнира
-suspend fun BehaviourContext.outputTournamentInfoMessage(message: ContentMessage<TextContent>, t: MutableTournament<*>) {
+// TODO если турнир уже начат — то таблица и матчи, которые сейчас играются, а также настройки, ссылка на полную таблицу
+suspend fun BehaviourContext.outputTournamentInfoMessage(message: ContentMessage<MessageContent>, t: MutableTournament<*>) {
     val players = t.getPlayersImmutable()
-    println("Выводим турнир")
-    sendMessage(message.chat, buildEntities("") {
-        +"Настройки турнира:\n" +
-                "• будет задействовано " + formatTournamentSetting("${t.tablesCnt} полей") + " (поменять: /$FIELDS_COUNT_COMMAND);\n" +
-                "• каждый сыграет по " + formatTournamentSetting("${t.tournamentMatchesPerPlayerCnt} матчей") + " (поменять: /$MATCHES_COUNT_COMMAND).\n" +
-                "\n" +
-                (if (players.isEmpty())
-                    "Участников нет. Добавьте их: /$ADD_PLAYER_COMMAND\n"
-                else
-                    "Участники (добавить: /$ADD_PLAYER_COMMAND, удалить: /$REMOVE_PLAYER_COMMAND):\n") +
-                t.getPlayersImmutable().joinToString("") { "• ${if (it.isPaused) "(пауза) " else ""}${it.name}\n" } +
-                "\n" +
-                "Любые настройки можно поменять как до, так и во время турнира.\n" +
-                "Когда всё будет готово, запустите турнир: /$START_TOURNAMENT_COMMAND."
-    })
+    if (t.isTournamentStarted()) {
+        sendMessage(message.chat, buildEntities("") {
+            +"" + code(t.generateCurrentRanking().outputRanking())
+        })
+    } else {
+        sendMessage(message.chat, buildEntities("") {
+            +"Настройки турнира:\n" +
+                    "• будет задействовано " + formatTournamentSetting("${t.tablesCnt} полей") + " (поменять: /$FIELDS_COUNT_COMMAND);\n" +
+                    "• каждый сыграет по " + formatTournamentSetting("${t.tournamentMatchesPerPlayerCnt} матчей") + " (поменять: /$MATCHES_COUNT_COMMAND).\n" +
+                    "\n" +
+                    (if (players.isEmpty())
+                        "Участников нет. Добавьте их: /$ADD_PLAYER_COMMAND\n"
+                    else
+                        "Участники (добавить: /$ADD_PLAYER_COMMAND, удалить: /$REMOVE_PLAYER_COMMAND):\n") +
+                    t.getPlayersImmutable().joinToString("") { "• ${if (it.isPaused) "(пауза) " else ""}${it.name}\n" } +
+                    "\n" +
+                    "Любые настройки можно поменять как до, так и во время турнира.\n" +
+                    "Когда всё будет готово, запустите турнир: /$START_TOURNAMENT_COMMAND."
+        })
+    }
 }
