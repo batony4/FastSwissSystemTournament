d96bde351961b4f2a0618dd0daf047855c0ddfe3
Anton Popovich <a@redmercury.dev>
2023-02-02 15:34:42 +0000

    Сменил порядок команд в помощи к боту.

--- telegramBot/src/main/kotlin/fastSwiss/telegramBot/CommandsList.kt
+++ telegramBot/src/main/kotlin/fastSwiss/telegramBot/CommandsList.kt
@@ -1,16 +1,16 @@
 package fastSwiss.telegramBot
 
 
-val CREATE_TOURNAMENT_COMMAND = "create_tournament"
-val FIELDS_COUNT_COMMAND = "fields_count"
-val MATCHES_COUNT_COMMAND = "matches_count"
-val ADD_PLAYER_COMMAND = "add_player"
-val REMOVE_PLAYER_COMMAND = "remove_player"
-val PAUSE_PLAYER_COMMAND = "pause_player"
-val UNPAUSE_PLAYER_COMMAND = "resume_player"
-val START_TOURNAMENT_COMMAND =
+const val CREATE_TOURNAMENT_COMMAND = "create_tournament"
+const val FIELDS_COUNT_COMMAND = "fields_count"
+const val MATCHES_COUNT_COMMAND = "matches_count"
+const val ADD_PLAYER_COMMAND = "add_player"
+const val REMOVE_PLAYER_COMMAND = "remove_player"
+const val START_TOURNAMENT_COMMAND =
     "go" // TODO запуск турнира (после этого будут предлагаться новые матчи в ответ на любое изменение в турнире, либо надпись "турнир завершён")
-val MATCH_RESULT_COMMAND = "result" // TODO реализовать
+const val MATCH_RESULT_COMMAND = "result" // TODO реализовать
+const val PAUSE_PLAYER_COMMAND = "pause_player"
+const val UNPAUSE_PLAYER_COMMAND = "resume_player"
 
 /**
  * Выводит список команд для регистрации в BotFather.
@@ -21,8 +21,8 @@ fun main() {
     println("$MATCHES_COUNT_COMMAND - Настроить количество матчей")
     println("$ADD_PLAYER_COMMAND - Добавить участника")
     println("$REMOVE_PLAYER_COMMAND - Удалить участника")
-    println("$PAUSE_PLAYER_COMMAND - Временно не назначать участника на новые матчи")
-    println("$UNPAUSE_PLAYER_COMMAND - Снова назначать участника на новые матчи")
     println("$START_TOURNAMENT_COMMAND - Запустить турнир")
     println("$MATCH_RESULT_COMMAND - Указать результат матча")
+    println("$PAUSE_PLAYER_COMMAND - Временно не назначать участника на новые матчи")
+    println("$UNPAUSE_PLAYER_COMMAND - Снова назначать участника на новые матчи")
 }
