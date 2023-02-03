30323533e0a0347c0220093731edc02adc556e27
Anton Popovich <a@redmercury.dev>
2023-02-03 21:43:02 +0000

    В каждом чате -- свой турнир.

--- telegramBot/src/main/kotlin/fastSwiss/telegramBot/BotMain.kt
+++ telegramBot/src/main/kotlin/fastSwiss/telegramBot/BotMain.kt
@@ -6,6 +6,7 @@ import dev.inmo.tgbotapi.extensions.behaviour_builder.triggers_handling.onComman
 import fastSwiss.api.MutableTournament
 import fastSwiss.api.tournamentTypes.topological.TopologicalPairSorter
 import fastSwiss.api.tournamentTypes.topological.TopologicalRanker
+import fastSwiss.api.tournamentTypes.topological.TopologicalRanking
 
 // TODO всё привести к этому словарю по всему коду. Сам словарь сохранить в отдельном файле.
 /*
@@ -27,9 +28,13 @@ suspend fun main() {
     val bot = telegramBot("5890510404:AAFiYNfuAco2e4d_6ODH6bvTkXrHnNOn7Tw")
 
     bot.buildBehaviourWithLongPolling {
-        var t = MutableTournament(RANKER, PAIR_SORTER)
+        val tournaments = mutableMapOf<Long, MutableTournament<TopologicalRanking>>()
         CommandsEnum.values().forEach { v ->
-            onCommand(v.commandName) { t = runDialog(it, t, v.dialog) }
+            onCommand(v.commandName) {
+                val chatId = it.chat.id.chatId
+                val t = tournaments.getOrDefault(chatId, MutableTournament(RANKER, PAIR_SORTER))
+                tournaments[chatId] = runDialog(it, t, v.dialog)
+            }
         }
     }.join()
 }
