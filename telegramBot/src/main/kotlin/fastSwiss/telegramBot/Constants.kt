85b0d8c4b507a7cfc9046b20cb67efda2d942657
Anton Popovich <a@redmercury.dev>
2023-02-04 23:43:37 +0000

    Поправил ссылку, чтобы она корректно работала в Телеграме (localhost как ссылка не форматируется).

--- telegramBot/src/main/kotlin/fastSwiss/telegramBot/Constants.kt
+++ telegramBot/src/main/kotlin/fastSwiss/telegramBot/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     // TODO убрать из кода, чтобы было только в файле
     const val TELEGRAM_BOT_TOKEN = "5890510404:AAFiYNfuAco2e4d_6ODH6bvTkXrHnNOn7Tw"
 
-    //    const val TABLE_URL_PREFIX = "https://fast-swiss.herokuapp.com/table/"
-    const val TABLE_URL_PREFIX = "http://localhost:8080/table/"
+//    const val TABLE_URL_PREFIX = "https://fast-swiss.herokuapp.com/table/"
+const val TABLE_URL_PREFIX = "http://127.0.0.1:8080/table/"
 }
