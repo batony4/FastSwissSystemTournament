1f5cae0655451857364f3eb0930bbb747772955c
Anton Popovich <a@redmercury.dev>
2023-02-06 13:29:47 +0000

    Уменьшил шрифт в таблице.

--- api/src/main/kotlin/fastSwiss/api/tournamentTypes/Ranking.kt
+++ api/src/main/kotlin/fastSwiss/api/tournamentTypes/Ranking.kt
@@ -18,7 +18,12 @@ interface Ranking {
     companion object {
 
         const val HTML_STYLE_MONOSPACE =
-            "<style>body { font-family: 'DejaVu Sans Mono', 'Fira Mono', 'Liberation Mono', Monaco, 'Courier New', Courier, monospace }</style>"
+            """
+                <style>body {
+                    font-family: 'DejaVu Sans Mono', 'Fira Mono', 'Liberation Mono', Monaco, 'Courier New', Courier, monospace;
+                    font-size: smaller;
+                }</style>
+            """
 
         fun outputMatrixForPlayer(res: StringBuilder, player: MutablePlayerState, allPlayersSorted: List<MutablePlayerState>) {
             val index = allPlayersSorted.indexOf(player)
