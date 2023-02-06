3e7478adc7eb97fc3e7e5b1c824c26c3d3d8b96f
Anton Popovich <a@redmercury.dev>
2023-02-06 00:06:14 +0000

    Фикс сборки.

--- telegramBot/build.gradle.kts
+++ telegramBot/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("fastSwiss.kotlin-application-conventions")
+    kotlin("jvm")
 }
 
 repositories {
@@ -7,6 +8,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation("org.apache.commons:commons-text")
     implementation(project(":api"))
     implementation("dev.inmo:tgbotapi:5.0.1")
@@ -22,4 +24,25 @@ application {
 
 tasks.jar {
     manifest.attributes["Main-Class"] = "fastSwiss.telegramBot.BotMainKt"
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree) // OR .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
+
+//tasks.jar {
+//    manifest {
+//        attributes["Main-Class"] = "fastSwiss.telegramBot.BotMainKt"
+//    }
+//    // This line of code recursively collects and copies all of a project's files
+//    // and adds them to the JAR itself. One can extend this task, to skip certain
+//    // files or particular types at will
+//    from { configurations.runtimeClasspath.map { it.isDirectory() ? it : zipTree(it) } }
+//}
+
+//tasks.jar {
+//    manifest.attributes["Main-Class"] = "fastSwiss.telegramBot.BotMainKt"
+//    from { configurations.compileClasspath.collect { it.isDirectory() ? it : zipTree(it) } }
+//}
